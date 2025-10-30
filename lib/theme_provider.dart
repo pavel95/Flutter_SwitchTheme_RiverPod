d5e10edbb29e237d562997e53b667c180d2e2477
Pavel Vlasák <pavelvlasak05@gmail.com>
2025-10-30 15:12:42 +0100

    done

--- lib/theme_provider.dart
+++ lib/theme_provider.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppThemeState extends ChangeNotifier {
+  static const _themePreferenceKey = 'isDarkModeEnabled';
+
   bool isDarkModeEnabled = false;
+  SharedPreferences? _preferences;
+
+  AppThemeState() {
+    _loadSavedTheme;
+  }
+
+  Future<void> _loadSavedTheme() async {
+    final preferences = await SharedPreferences.getInstance();
+    _preferences = preferences;
+
+    isDarkModeEnabled =
+        preferences.getBool(_themePreferenceKey) ?? isDarkModeEnabled;
+    notifyListeners();
+  }
+
+  Future<void> _persistThemePreference(bool isDarkModeEnabled) async {
+    final preferences = _preferences ??= await SharedPreferences.getInstance();
+    await preferences.setBool(_themePreferenceKey, isDarkModeEnabled);
+  }
+
   void setLightTheme() {
     isDarkModeEnabled = false;
     notifyListeners();
+    _persistThemePreference(false);
   }
 
   void setDarkTheme() {
     isDarkModeEnabled = true;
     notifyListeners();
+    _persistThemePreference(true);
   }
 }
 
